05619cc3a7b877d7745807b675fea296cabaf930
davquipe <davidcondori99@gmail.com>
2022-09-23 16:12:56 -0500

    Rapid API Music Fetching

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'd2ac3d15c6msh50d17591ef2b24fp1eb393jsn6fcc0d859c08',
+    'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com',
+  },
+};
+
+fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        // eslint-disable-next-line comma-dangle
+        'd2ac3d15c6msh50d17591ef2b24fp1eb393jsn6fcc0d859c08'
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
